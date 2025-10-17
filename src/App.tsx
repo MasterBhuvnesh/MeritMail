b2d5467be6aeeb9dfd8f07013b4ec6cd8fe0beb3
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2025-10-17 22:37:57 +0530

    feat: initialize project with Vite, React, and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p className="mt-4 text-center text-3xl font-medium text-gray-800">
+        GDG - MeritMail - A desktop application for managing your emails with
+        ease and efficiency.
+      </p>
+    </>
+  );
+}
+
+export default App;
