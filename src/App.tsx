6889ea9528f30d8cff4002056b0b98fe379047de
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2025-10-17 22:48:02 +0530

    feat: add Product Sans font and update styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <p className="mt-4 text-center text-3xl font-medium text-gray-800">
+      <p className="font-product mt-4 text-center text-3xl text-gray-800">
         GDG - MeritMail - A desktop application for managing your emails with
         ease and efficiency.
       </p>
